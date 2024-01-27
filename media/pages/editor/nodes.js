417af3a26653881f0a2156cb630670b65de30d10
Lime <demn2001@gmail.com>
2024-01-27 05:23:06 +0300

    change from `param-based` to `object-based` setup

--- media/pages/editor/nodes.js
+++ media/pages/editor/nodes.js
@@ -2,7 +2,7 @@
 
 /**
  * @typedef {Object} NodeTemplate
- * @property {string} icon
+ * @property {string | undefined} icon
  * @property {string} background
  * @property {Object.<string, NodePort>} [inports]
  * @property {Object.<string, NodePort>} [outports]
@@ -18,17 +18,18 @@
 class Nodes {
 	/**
 	 * @param {string} name
-	 * @param {boolean} leftside
+	 * @param {NodePort} data
+	 * @param {boolean} isInput
 	 */
-	static #makePort(name, leftside) {
-        const port = new go.Shape("Rectangle", {
-            fill: "gray",
+	static #makePort(name, data, isInput) {
             stroke: null,
             desiredSize: new go.Size(8, 8),
             portId: name,
             toMaxLinks: 1,
             cursor: "pointer",
         });
+		// @ts-ignore
+		port.rawData = data;
         const lab = new go.TextBlock(name, { // the name of the port
             font: "7pt sans-serif"
         });
@@ -36,7 +37,7 @@ class Nodes {
             margin: new go.Margin(2, 0)
         });
 		// set up the port/panel based on which side of the node it will be on
-		if (leftside) {
+		if (isInput) {
 			port.toSpot = go.Spot.Left;
 			port.toLinkable = true;
 			lab.margin = new go.Margin(1, 0, 0, 1);
@@ -57,14 +58,12 @@ class Nodes {
 	/**
      * @param {go.Diagram} editor 
 	 * @param {string} typename
-	 * @param {string} icon
-	 * @param {string} background
-	 * @param {go.GraphObject[]} inports
-	 * @param {go.GraphObject[]} outports
+	 * @param {NodeTemplate} data
      * @returns {go.Node}
 	 */
-	static #makeTemplate(editor, typename, icon, background, inports, outports) {
+	static #makeTemplate(editor, typename, data) {
 		const node =  new go.Node("Spot", {
+			name: typename
 			//margin: 5,
 			//background: "red"
 		})
@@ -74,7 +73,7 @@ class Nodes {
 				height: 120,
 			})
 			.add(new go.Shape("Rectangle", {
-				fill: background,
+				fill: data.background,
 				stroke: null,
 				strokeWidth: 0,
 				spot1: go.Spot.TopLeft,
@@ -89,7 +88,7 @@ class Nodes {
 					stroke: "black",
 					font: "bold 12pt sans-serif"
 				}))
-				.add(new go.Picture(icon, {
+				.add(new go.Picture(data.icon, {
 					row: 1,
 
 					width: 16,
@@ -110,57 +109,15 @@ class Nodes {
 		.add(new go.Panel("Vertical", {
 			alignment: go.Spot.Left,
 			alignmentFocus: new go.Spot(0, 0.5, 8, 0)
-		}).add(...inports))
+		}).add(...Object.entries(data.inports ?? {})?.map(v => Nodes.#makePort(v[0], v[1], true))))
 		.add(new go.Panel("Vertical", {
 			alignment: go.Spot.Right,
 			alignmentFocus: new go.Spot(1, 0.5, -8, 0)
-		}).add(...outports));
-/*
-		var node = $(go.Node, "Spot",
-			$(go.Panel, "Auto",
-				{ width: 100, height: 120 },
-				$(go.Shape, "Rectangle",
-					{
-						fill: background, stroke: null, strokeWidth: 0,
-						spot1: go.Spot.TopLeft, spot2: go.Spot.BottomRight
-					}),
-				$(go.Panel, "Table",
-					$(go.TextBlock, typename,
-						{
-							row: 0,
-							margin: 3,
-							maxSize: new go.Size(80, NaN),
-							stroke: "black",
-							font: "bold 12pt sans-serif"
-						}),
-					$(go.Picture, icon,
-						{ row: 1, width: 16, height: 16, scale: 3.0 }),
-					$(go.TextBlock,
-						{
-							row: 2,
-							margin: 3,
-							editable: true,
-							maxSize: new go.Size(80, 40),
-							stroke: "white",
-							font: "bold 9pt sans-serif"
-						},
-						new go.Binding("text", "name").makeTwoWay())
-				)
-			),
-			$(go.Panel, "Vertical",
-				{
-					alignment: go.Spot.Left,
-					alignmentFocus: new go.Spot(0, 0.5, 8, 0)
-				},
-				inports),
-			$(go.Panel, "Vertical",
-				{
-					alignment: go.Spot.Right,
-					alignmentFocus: new go.Spot(1, 0.5, -8, 0)
-				},
-				outports)
-		);
-*/
+		}).add(...Object.entries(data.outports ?? {})?.map(v => Nodes.#makePort(v[0], v[1], false))));
+
+		// @ts-ignore
+		node.rawData = data;
+
 		editor.nodeTemplateMap.set(typename, node);
         return node;
 	}
@@ -171,14 +128,7 @@ class Nodes {
      */
     static make(editor, templates) {
         for (const [typename, template] of Object.entries(templates)) {
-            this.#makeTemplate(
-                editor, 
-                typename, 
-                template.icon,
-                template.background, 
-                Object.entries(template.inports ?? {})?.map(v => this.#makePort(v[0], true)),
-                Object.entries(template.outports ?? {})?.map(v => this.#makePort(v[0], false)),
-            );
+            Nodes.#makeTemplate(editor, typename, template);
         }
     }
 }
