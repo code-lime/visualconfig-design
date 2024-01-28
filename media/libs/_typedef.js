b3a6729d8e418417d12669b65771a4fd3df15d46
Lime <demn2001@gmail.com>
2024-01-28 05:46:16 +0300

    add `NodeField` types

--- media/libs/_typedef.js
+++ media/libs/_typedef.js
@@ -20,3 +20,64 @@
  * @template T
  * @typedef {{validate:ValidateFunc<T>,getter:GetterFunc<T>,setter:SetterFunc<T>}} ValidationElement
  */
+
+/**
+ * @typedef {'integer' | 'double' | 'color' | 'text' | 'select'} TypeNodeField
+ */
+
+/**
+ * @typedef {Object} BaseNodeField
+ * @property {string} name
+ * @property {boolean} readonly
+ * @property {TypeNodeField} type
+ * @property {number | string} value
+ * @property {string | undefined} [group]
+ */
+
+/**
+ * @typedef {NodeFieldInteger | NodeFieldDouble | NodeFieldColor | NodeFieldText | NodeFieldSelect} AnyNodeField
+ */
+
+/**
+ * @typedef {Object} _NodeFieldInteger
+ * @property {'integer'} type
+ * @property {number} value
+ * 
+ * @typedef {BaseNodeField & _NodeFieldInteger} NodeFieldInteger 
+ */
+/**
+ * @typedef {Object} _NodeFieldDouble
+ * @property {'double'} type
+ * @property {number} value
+ * 
+ * @typedef {BaseNodeField & _NodeFieldDouble} NodeFieldDouble 
+ */
+/**
+ * @typedef {Object} _NodeFieldColor
+ * @property {'color'} type
+ * @property {string} value
+ * 
+ * @typedef {BaseNodeField & _NodeFieldColor} NodeFieldColor 
+ */
+/**
+ * @typedef {Object} _NodeFieldText
+ * @property {'text'} type
+ * @property {string} value
+ * 
+ * @typedef {BaseNodeField & _NodeFieldText} NodeFieldText 
+ */
+/**
+ * @typedef {Object} _NodeFieldSelect
+ * @property {'select'} type
+ * @property {string} value
+ * @property {Array<string>} list
+ * 
+ * @typedef {BaseNodeField & _NodeFieldSelect} NodeFieldSelect 
+ */
+
+/**
+ * @typedef {Object} NodeElement
+ * @property {number} id
+ * @property {string} type
+ * @property {Object.<string, AnyNodeField>} fields
+ */
